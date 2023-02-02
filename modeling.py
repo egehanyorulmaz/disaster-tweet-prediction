1eae60f2c7357c2034f40820f1e6794c73a60f95
egehanyorulmaz <egehanyorulmaz@uchicago.edu>
2023-02-01 21:52:38 -0600

    organizes the model development codes to one file

--- modeling.py
+++ modeling.py
@@ -5,31 +5,47 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 
 from helpers.basic_models import fit_model
+from helpers.utils import save_model
 
 df = pd.read_csv('data/train_preprocessed.csv')
 
 print('Splitting the data...')
-X_train, X_test, y_train, y_test = train_test_split(df['text'], df['target'], test_size=0.2, random_state=42)
+y = df['target']
+X = df.drop(columns=['id', 'location', 'target'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 print('Data is successfully splitted.')
 
+print('Fitting the NB model...')
 nb = MultinomialNB()
 count_vec2 = CountVectorizer(stop_words='english', ngram_range=(1, 3))
+count_vectorizer1, nb = fit_model(nb, count_vec2,
+                                  X_train.drop(columns=['keyword']),
+                                  X_test.drop(columns=['keyword']), y_train, y_test,
+                                  column='text',
+                                  get_feature_importance=False)
 
-print('Fitting the model...')
-count_vectorizer1, nb = fit_model(nb, count_vec2, X_train, X_test, y_train, y_test, get_feature_importance=True)
-
+print('Fitting the LogisticRegression model...')
 logreg = LogisticRegression(max_iter=10000)
-count_vectorizer2, logreg = fit_model(logreg, count_vec2, X_train, X_test, y_train, y_test, get_feature_importance=True)
+count_vectorizer2, logreg = fit_model(logreg, count_vec2,
+                                      X_train.drop(columns=['keyword']),
+                                      X_test.drop(columns=['keyword']), y_train, y_test,
+                                      column='text',
+                                      get_feature_importance=False)
 
-df = pd.read_csv('data/train_preprocessed.csv')
-x_test = count_vectorizer1.transform(df['text'])
-predictions = logreg.predict(x_test)
-df = pd.DataFrame({'id': df['id'], 'target': predictions})
-df.to_csv('data/submission.csv', index=False)
+print('Fitting the LogisticRegression with Keyword column')
+logreg2 = LogisticRegression(max_iter=10000)
+count_vec3 = CountVectorizer()
+count_vectorizer3, logreg2 = fit_model(logreg2, count_vec3,
+                                       X_train.drop(columns=['text']),
+                                       X_test.drop(columns=['text']), y_train, y_test,
+                                       column='keyword',
+                                       get_feature_importance=True)
 
-a = 5
-
-
-
-
-
+# save the model
+save_model(count_vectorizer1, '/models/count_vectorizer1.pkl')
+save_model(count_vectorizer2, '/models/count_vectorizer2.pkl')
+save_model(count_vectorizer3, '/models/count_vectorizer3.pkl')
+save_model(logreg, '/models/logreg.pkl')
+save_model(logreg2, '/models/logreg2.pkl')
+save_model(nb, '/models/nb.pkl')
+print('Models are successfully saved.')
